ae88728ed2d16e404ed57c2e0a7e19fe0ff80d11
raonarvokshi <raonar.vokshi@gmail.com>
2024-07-02 20:18:06 +0200

    Final Results

---
+++ main.py
@@ -0,0 +1,66 @@
+import json
+import os
+import time
+from colorama import Fore
+from students import Students
+
+program_on = True
+
+
+Students.load_students_from_file()
+
+
+def start_program():
+    global program_on
+    while program_on:
+        try:
+            print("1. Add Student")
+            print("2. Show Students")
+            print("3. Search Student By Name")
+            print("4. Search Student By Age")
+            print("5. Search Student By Subject")
+            options = int(input("Choose one of the options: "))
+            Students.clear_screen()
+
+            if options == 1:
+                student_name = input("Enter student name: ")
+                student_age = int(input("Enter student age: "))
+                student_subject = input("Enter student subject: ")
+                student = Students(name=student_name, age=student_age,
+                                   subject=student_subject)
+                student.add_student()
+            elif options == 2:
+                Students.show_all_students()
+
+            elif options == 3:
+                Students.filter_student_by_name()
+
+            elif options == 4:
+                Students.filter_student_by_age()
+
+            elif options == 5:
+                Students.filter_student_by_subject()
+
+            else:
+                print(f"{Fore.RED}Invalid option.")
+
+        except ValueError:
+            Students.clear_screen()
+            print(f"{Fore.RED}Enter an option from 1 to 4!")
+            time.sleep(3)
+
+        print(
+            f"{Fore.RESET}Do you want to continue using the program or quit the program")
+        continue_program = input(
+            f"{Fore.RESET}Enter 'continue' or 'quit': ").lower()
+        Students.clear_screen()
+
+        if continue_program != "continue":
+            program_on = False
+            # Save students to file when the program ends
+            Students.save_students_to_file()
+            print(f"{Fore.GREEN}Program quit successfully")
+            break
+
+
+start_program()
---
+++ students.py
@@ -0,0 +1,130 @@
+from colorama import Fore
+import time
+import os
+import json
+
+
+class Students:
+    students = []
+
+    def __init__(self, name, age, subject):
+        self.name = name
+        self.age = age
+        self.subject = subject
+
+    def add_student(self):
+        new_student = {
+            "name": self.name,
+            "age": self.age,
+            "subject": self.subject
+        }
+        Students.students.append(new_student)
+        print("Student was added successfully!\n")
+
+    @classmethod
+    def show_all_students(cls):
+        if not cls.students:
+            print(f"{Fore.LIGHTBLUE_EX}Loading students...")
+            time.sleep(3)
+            cls.clear_screen()
+
+            print(f"{Fore.RED}No students to show.")
+        else:
+            print(f"{Fore.LIGHTBLUE_EX}Loading students...")
+            time.sleep(3)
+            cls.clear_screen()
+            for student in cls.students:
+                print(f"{Fore.GREEN}{student}\n")
+
+    @classmethod
+    def filter_student_by_name(cls):
+        filter_name = input("Enter student name: ")
+        cls.clear_screen()
+
+        print(f"{Fore.LIGHTBLUE_EX}Loading student...")
+        time.sleep(3)
+        cls.clear_screen()
+
+        found_students = []
+        for student in cls.students:
+            if student["name"] == filter_name:
+                found_students.append(student)
+
+        if found_students:
+            if len(found_students) == 1:
+                print(
+                    f"{Fore.CYAN}Found {len(found_students)} student with the name {filter_name}:\n")
+            else:
+                print(
+                    f"{Fore.CYAN}Found {len(found_students)} students with the name {filter_name}:\n")
+
+            for student in found_students:
+                for key, value in student.items():
+                    print(f"{Fore.GREEN}{key}: {value}")
+                print("\n")
+        else:
+            print(f"{Fore.RED}No student found with the name {filter_name}.")
+
+    @classmethod
+    def filter_student_by_age(cls):
+        from_age = int(input(f"{Fore.WHITE}Enter from age: "))
+        to_age = int(input(f"{Fore.WHITE}Enter to age: "))
+        cls.clear_screen()
+        filtered_students = [
+            student for student in cls.students if from_age <= student["age"] <= to_age]
+
+        if not filtered_students:
+            print(f"{Fore.LIGHTBLUE_EX}Loading student...\n")
+            time.sleep(2)
+            cls.clear_screen()
+            print(
+                f"{Fore.RED}No students found in the age range {from_age} to {to_age}.")
+        else:
+            print(f"{Fore.LIGHTBLUE_EX}Loading student...\n")
+            time.sleep(3)
+            cls.clear_screen()
+            print(
+                f"{Fore.CYAN}Students in the age range {from_age} to {to_age}:\n")
+            for student in filtered_students:
+                print(f"{Fore.GREEN}{student}\n")
+
+    @classmethod
+    def filter_student_by_subject(cls):
+        stud_subj = input("Enter student subject: ")
+        cls.clear_screen()
+
+        filtered_students = [
+            student for student in cls.students if student["subject"] == stud_subj]
+
+        if not filtered_students:
+            print(f"{Fore.LIGHTBLUE_EX}Loading students...")
+            time.sleep(3)
+            cls.clear_screen()
+
+            print(f"{Fore.RED}No students found with the subject: {stud_subj}\n")
+        else:
+            print(f"{Fore.LIGHTBLUE_EX}Loading students...")
+            time.sleep(3)
+            cls.clear_screen()
+
+            print(f"{Fore.CYAN}Students with the subject: {stud_subj}\n")
+            for student in filtered_students:
+                print(f"{Fore.GREEN}{student}\n")
+
+    @classmethod
+    def save_students_to_file(cls, filename="students.json"):
+        with open(filename, 'w') as file:
+            json.dump(cls.students, file)
+
+    @classmethod
+    def load_students_from_file(cls, filename="students.json"):
+        try:
+            with open(filename, 'r') as file:
+                cls.students = json.load(file)
+        except FileNotFoundError:
+            print(f"{Fore.RED}No saved students found.\n")
+
+    @staticmethod
+    def clear_screen():
+        # Clear the terminal screen
+        os.system('cls' if os.name == 'nt' else 'clear')
